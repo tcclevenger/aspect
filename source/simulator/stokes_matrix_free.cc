d382312fb3dc03858a3743f90a00591acde54d66
tcclevenger <tcleven@clemson.edu>
2019-09-17 16:47:13 -0400

    update

--- source/simulator/stokes_matrix_free.cc
+++ source/simulator/stokes_matrix_free.cc
@@ -1912,7 +1912,7 @@ namespace aspect
 
     sim.pcout << "Matrix-vector Product Timings:   " << matvec << std::endl;
 
-    internal::ChangeVectorTypes::copy(tmp4,distributed_stokes_rhs);
+    internal::ChangeVectorTypes::copy(tmp4,distributed_stokes_solution);
     timer.restart();
     for (unsigned int i=0; i<n_prec; ++i)
       {
