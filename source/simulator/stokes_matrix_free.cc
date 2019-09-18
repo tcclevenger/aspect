482a96eb6b4e91bba1fabbcec09fb53694793fbd
tcclevenger <tcleven@clemson.edu>
2019-09-18 09:11:32 -0400

    change order

--- source/simulator/stokes_matrix_free.cc
+++ source/simulator/stokes_matrix_free.cc
@@ -1783,6 +1783,31 @@ namespace aspect
     unsigned int bicgstab_m = 0.0;
     try
       {
+        SolverBicgstab<dealii::LinearAlgebra::distributed::BlockVector<double>> solver(solver_control_cheap);
+
+        solution_copy = 0;
+        timer.restart();
+        solver.solve(stokes_matrix,
+                     solution_copy,
+                     rhs_copy,
+                     preconditioner_cheap);
+        timer.stop();
+        const double solve_time = timer.last_wall_time();
+        bicgstab_m = solver_control_cheap.last_step();
+        sim.pcout << "   BiCGStab Solved in " << bicgstab_m << " iterations (" << solve_time << "s)."
+                  << std::endl;
+      }
+    catch (SolverControl::NoConvergence)
+      {
+//        sim.pcout << "********************************************************************" << std::endl
+//                  << "BiCGStab DID NOT CONVERGE AFTER "
+//                  << solver_control_cheap.last_step()
+//                  << " ITERATIONS. res=" << solver_control_cheap.last_value() << std::endl
+//                  << "********************************************************************" << std::endl;
+      }
+
+    try
+      {
         SolverFGMRES<dealii::LinearAlgebra::distributed::BlockVector<double> >
         solver(solver_control_cheap, mem,
                SolverFGMRES<dealii::LinearAlgebra::distributed::BlockVector<double> >::
@@ -1835,30 +1860,7 @@ namespace aspect
 //                  << "********************************************************************" << std::endl;
       }
 
-    try
-      {
-        SolverBicgstab<dealii::LinearAlgebra::distributed::BlockVector<double>> solver(solver_control_cheap);
 
-        solution_copy = 0;
-        timer.restart();
-        solver.solve(stokes_matrix,
-                     solution_copy,
-                     rhs_copy,
-                     preconditioner_cheap);
-        timer.stop();
-        const double solve_time = timer.last_wall_time();
-        bicgstab_m = solver_control_cheap.last_step();
-        sim.pcout << "   BiCGStab Solved in " << bicgstab_m << " iterations (" << solve_time << "s)."
-                  << std::endl;
-      }
-    catch (SolverControl::NoConvergence)
-      {
-//        sim.pcout << "********************************************************************" << std::endl
-//                  << "BiCGStab DID NOT CONVERGE AFTER "
-//                  << solver_control_cheap.last_step()
-//                  << " ITERATIONS. res=" << solver_control_cheap.last_value() << std::endl
-//                  << "********************************************************************" << std::endl;
-      }
 
     const unsigned int n_scalar = 1000;
     const unsigned int n_matvec = 100;
